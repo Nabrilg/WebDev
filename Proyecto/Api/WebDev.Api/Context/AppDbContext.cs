df5b3fd322dd64744cd154603fca1fc1b70658b9
Laura Arango Mayor <lauarangom@gmail.com>
2020-10-24 11:19:55 -0500

    Sesion-02-Version Inicial

---
+++ Proyecto/Api/WebDev.Api/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace WebDev.Api.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using  Model; //Saves writing
+
+    public class AppDbContext : DbContext
+    {
+        #region Initialization
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        #endregion Initialization
+
+        #region Properties
+        public DbSet<User> Users { get; set; }
+        #endregion
+    }
+}
