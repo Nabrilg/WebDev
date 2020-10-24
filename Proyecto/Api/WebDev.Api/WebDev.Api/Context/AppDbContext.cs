1b6b9a18a41164047ae16d2c48bc3e63f90aad36
DESKTOP-8A1G7ND\totor <totoroldan12@gmail.com>
2020-10-24 14:06:18 -0500

    Sesion-02-Version Inicial

---
+++ Proyecto/Api/WebDev.Api/WebDev.Api/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDev.api.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using WebDev.api.Model;
+    using WebDev.api.Model;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
