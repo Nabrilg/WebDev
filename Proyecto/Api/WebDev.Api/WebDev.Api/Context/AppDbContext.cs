afe15d4d2c641a8f169dab433126e77257c0cc55
Andres Felipe Salazar Rojas <andres96@javerianacali.edu.co>
2020-11-06 18:25:30 -0500

    commit

---
+++ Proyecto/Api/WebDev.Api/WebDev.Api/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace WebDev.Api.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using Model;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Proyecto/Api/WebDev.Api/WebDev.Api/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDev.Api.Model
+{
+    using System.ComponentModel.DataAnnotations;
+    public class User
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Email { get; set; }
+        [Required]
+        public int Name { get; set; }
+        [Required]
+        public int Password { get; set; }
+        [Required]
+        public int Username { get; set; }
+    }
+}
