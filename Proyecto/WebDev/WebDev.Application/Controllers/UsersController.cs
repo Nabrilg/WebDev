f7493bfe3e96f77de591eb47c7131cef56d4e1fe
Laura Arango Mayor <lauarangom@gmail.com>
2020-11-14 11:39:25 -0500

    Connect Web-Api

--- Proyecto/WebDev/WebDev.Application/Controllers/UsersController.cs
+++ Proyecto/WebDev/WebDev.Application/Controllers/UsersController.cs
@@ -1,54 +1,66 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using WebDev.Application.Config;
 using WebDev.Application.Models;
+using WebDev.Services;
+using WebDev.Services.Entities;
 
 namespace WebDev.Application.Controllers
 {
     public class UsersController : Controller
     {
         #region Properties
-
         private static List<User> userList;
         private static int numUsers;
-
+        private readonly ApiConfiguration apiConfiguration;
+        private UsersService usersService;
         #endregion
 
-        public UsersController()
+        public UsersController(IOptions<ApiConfiguration> apiConfig)
         {
-            // Mock User List
-            if (userList is null)
-            {
-                userList = new List<User>()
-                {
-                    new User{Id=1, Email="Julio.Robles@email.com", Name="Julio Robles", Username="jrobles", Password="Password"},
-                    new User{Id=2, Email="Pilar.Lopez@email.com", Name="Pilar Lopez", Username="plopez", Password="Password"},
-                    new User{Id=3, Email="Felipe.Daza@email.com", Name="Felipe Daza", Username="fdaza", Password="Password"},
-                };
-                numUsers = userList.Count;
-            }
+            apiConfiguration = apiConfig.Value;
+            usersService = new UsersService(apiConfiguration.ApiUsersUrl);
+            //// Mock User List
+            //if (userList is null)
+            //{
+            //    userList = new List<User>()
+            //    {
+            //        new User{Id=1, Email="Julio.Robles@email.com", Name="Julio Robles", Username="jrobles", Password="Password"},
+            //        new User{Id=2, Email="Pilar.Lopez@email.com", Name="Pilar Lopez", Username="plopez", Password="Password"},
+            //        new User{Id=3, Email="Felipe.Daza@email.com", Name="Felipe Daza", Username="fdaza", Password="Password"},
+            //    };
+            //    numUsers = userList.Count;
+            //}
         }
 
         // GET: UsersController
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             // Set Object Model
+            IList<UserDto> users = await usersService.GetUsers();
+
+            userList = users.Select(userDto => MapperToUser(userDto)).ToList();
+
             return View(userList);
         }
 
         // GET: UsersController/Details/5
         [HttpGet]
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(int id)
         {
-            var userFound = userList.FirstOrDefault(u => u.Id == id);
+            var userFound = await usersService.GetUserById(id);
 
             if (userFound == null)
             {
                 return NotFound();
             }
 
-            return View(userFound);
+            var user = MapperToUser(userFound);
+
+            return View(user);
         }
 
         // GET: UsersController/Create
@@ -61,14 +73,13 @@ namespace WebDev.Application.Controllers
         // POST: UsersController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(User user)
+        public async Task<ActionResult> Create(User user)
         {
             try
             {
                 if (ModelState.IsValid)
                 {
-                    user.Id = ++numUsers;
-                    userList.Add(user);
+                    var userAdded = await usersService.AddUser(MapperToUserDto(user));
                 }
 
                 return RedirectToAction(nameof(Index));
@@ -81,32 +92,30 @@ namespace WebDev.Application.Controllers
 
         // GET: UsersController/Edit/5
         [HttpGet]
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(int id)
         {
-            var userFound = userList.FirstOrDefault(u => u.Id == id);
+            var userFound = await usersService.GetUserById(id);
 
             if (userFound == null)
             {
                 return NotFound();
             }
 
-            return View(userFound);
+            var user = MapperToUser(userFound);
+
+            return View(user);
         }
 
         // POST: UsersController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(User user)
+        public async Task<ActionResult> Edit(User user)
         {
             try
             {
                 if (ModelState.IsValid)
                 {
-                    var userFound = userList.FirstOrDefault(u => u.Id == user.Id);
-                    userFound.Email = user.Email;
-                    userFound.Name = user.Name;
-                    userFound.Username = user.Username;
-                    userFound.Password = user.Password;
+                    var userModified = await usersService.UpdateUser(MapperToUserDto(user));
 
                     return RedirectToAction(nameof(Index));
                 }
@@ -120,39 +129,65 @@ namespace WebDev.Application.Controllers
 
         // GET: UsersController/Delete/5
         [HttpGet]
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            var userFound = userList.FirstOrDefault(u => u.Id == id);
+            var userFound = await usersService.GetUserById(id);
 
             if (userFound == null)
             {
                 return NotFound();
             }
 
-            return View(userFound);
+            var user = MapperToUser(userFound);
+
+            return View(user);
         }
 
         // POST: UsersController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(User user)
+        public async Task<ActionResult> Delete(User user)
         {
             try
             {
-                var userFound = userList.FirstOrDefault(u => u.Id == user.Id);
+                var userFound = await usersService.GetUserById(user.Id);
 
                 if (userFound == null)
                 {
                     return View();
                 }
 
-                userList.Remove(userFound);
+                var userDeleted = await usersService.DeleteUser(user.Id);
+
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
                 return View();
             }
+        }
+
+        private User MapperToUser(UserDto userDto)
+        {
+            return new User
+            {
+                Id = userDto.Id,
+                Email = userDto.Email,
+                Name = userDto.Name,
+                Username = userDto.Username,
+                Password = userDto.Password
+            };
+        }
+
+        private UserDto MapperToUserDto(User user)
+        {
+            return UserDto.Build(
+              id: user.Id,
+              email: user.Email,
+              name: user.Name,
+              username: user.Username,
+              password: user.Password
+            );
         }
     }
 }
---
+++ Proyecto/WebDev/WebDev.Services/UsersService.cs
@@ -0,0 +1,148 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebDev.Services.Entities;
+using System;
+
+namespace WebDev.Services
+{
+    public class UsersService
+    {
+        #region Properties
+        private string BaseUrl { get; }
+        private HttpClient httpClient;
+        private HttpClientHandler httpClientHandler;
+        #endregion
+
+        #region Initialize
+        public UsersService(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+            httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            httpClient = new HttpClient(httpClientHandler);
+            SetupHttpConnection(httpClient, baseUrl);
+        }
+        #endregion
+
+        #region Methods
+        private void SetupHttpConnection(HttpClient httpClient, string baseUrl)
+        {
+            //Passing service base url  
+            httpClient.BaseAddress = new Uri(baseUrl);
+
+            httpClient.DefaultRequestHeaders.Clear();
+
+            //Define request data format  
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<UserDto>> GetUsers()
+        {
+            var usersList = new List<UserDto>();
+
+            // Sending request to find web api REST service resource to Get All Users using HttpClient
+            HttpResponseMessage response = await httpClient.GetAsync("api/Users");
+
+            // Checking the response is successful or not which is sent using HttpClient
+            if (response.IsSuccessStatusCode)
+            {
+                // Storing the content response recieved from web api
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+
+                //Deserializing the response recieved from web api and storing into the Employee list
+                usersList = JsonConvert.DeserializeObject<List<UserDto>>(responseContent);
+            }
+
+            return usersList;
+        }
+
+        public async Task<UserDto> GetUserById(int id)
+        {
+            UserDto user = null;
+
+            // Sending request to find web api REST service resource to Get All Users using HttpClient
+            HttpResponseMessage response = await httpClient.GetAsync($"api/Users/{id}");
+
+            // Checking the response is successful or not which is sent using HttpClient
+            if (response.IsSuccessStatusCode)
+            {
+                // Storing the content response recieved from web api
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+
+                //Deserializing the response recieved from web api and storing into the Employee list
+                user = JsonConvert.DeserializeObject<UserDto>(responseContent);
+            }
+
+            return user;
+        }
+
+        public async Task<UserDto> AddUser(UserDto user)
+        {
+            UserDto userDtoResponse = null;
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+            // Sending request to find web api REST service resource to Add an User using HttpClient
+            HttpResponseMessage response = await httpClient.PostAsync($"api/Users", content);
+
+            // Checking the response is successful or not which is sent using HttpClient
+            if (response.IsSuccessStatusCode)
+            {
+                // Storing the content response recieved from web api
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+
+                //Deserializing the response recieved from web api and storing into the Employee list
+                userDtoResponse = JsonConvert.DeserializeObject<UserDto>(responseContent);
+            }
+
+            return userDtoResponse;
+        }
+
+        public async Task<UserDto> UpdateUser(UserDto user)
+        {
+            UserDto userDtoResponse = null;
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+            // Sending request to find web api REST service resource to Add an User using HttpClient
+            HttpResponseMessage response = await httpClient.PutAsync($"api/Users/{user.Id}", content);
+
+            // Checking the response is successful or not which is sent using HttpClient
+            if (response.IsSuccessStatusCode)
+            {
+                // Storing the content response recieved from web api
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+
+                //Deserializing the response recieved from web api
+                userDtoResponse = JsonConvert.DeserializeObject<UserDto>(responseContent);
+            }
+
+            return userDtoResponse;
+        }
+
+        public async Task<UserDto> DeleteUser(int id)
+        {
+            UserDto userDtoResponse = null;
+
+            // Sending request to find web api REST service resource to Delete the User using HttpClient
+            HttpResponseMessage response = await httpClient.DeleteAsync($"api/Users/{id}");
+
+            // Checking the response is successful or not which is sent using HttpClient
+            if (response.IsSuccessStatusCode)
+            {
+                // Storing the content response recieved from web api
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+
+                // Deserializing the response recieved from web api
+                userDtoResponse = JsonConvert.DeserializeObject<UserDto>(responseContent);
+            }
+
+            return userDtoResponse;
+        }
+        #endregion
+    }
+}
