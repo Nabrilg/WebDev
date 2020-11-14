3671b0f0ae3a7d61557f4e72a3a86321bb4a4635
Laura Arango Mayor <lauarangom@gmail.com>
2020-11-14 09:11:58 -0500

    Mocks api

---
+++ Proyecto/WebDev/WebDev.Application/Controllers/UsersController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebDev.Application.Models;
+
+namespace WebDev.Application.Controllers
+{
+    public class UsersController : Controller
+    {
+        #region Properties
+
+        private static List<User> userList;
+        private static int numUsers;
+
+        #endregion
+
+        public UsersController()
+        {
+            // Mock User List
+            if (userList is null)
+            {
+                userList = new List<User>()
+                {
+                    new User{Id=1, Email="Julio.Robles@email.com", Name="Julio Robles", Username="jrobles", Password="Password"},
+                    new User{Id=2, Email="Pilar.Lopez@email.com", Name="Pilar Lopez", Username="plopez", Password="Password"},
+                    new User{Id=3, Email="Felipe.Daza@email.com", Name="Felipe Daza", Username="fdaza", Password="Password"},
+                };
+                numUsers = userList.Count;
+            }
+        }
+
+        // GET: UsersController
+        public ActionResult Index()
+        {
+            // Set Object Model
+            return View(userList);
+        }
+
+        // GET: UsersController/Details/5
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var userFound = userList.FirstOrDefault(u => u.Id == id);
+
+            if (userFound == null)
+            {
+                return NotFound();
+            }
+
+            return View(userFound);
+        }
+
+        // GET: UsersController/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    user.Id = ++numUsers;
+                    userList.Add(user);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var userFound = userList.FirstOrDefault(u => u.Id == id);
+
+            if (userFound == null)
+            {
+                return NotFound();
+            }
+
+            return View(userFound);
+        }
+
+        // POST: UsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var userFound = userList.FirstOrDefault(u => u.Id == user.Id);
+                    userFound.Email = user.Email;
+                    userFound.Name = user.Name;
+                    userFound.Username = user.Username;
+                    userFound.Password = user.Password;
+
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(user);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var userFound = userList.FirstOrDefault(u => u.Id == id);
+
+            if (userFound == null)
+            {
+                return NotFound();
+            }
+
+            return View(userFound);
+        }
+
+        // POST: UsersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(User user)
+        {
+            try
+            {
+                var userFound = userList.FirstOrDefault(u => u.Id == user.Id);
+
+                if (userFound == null)
+                {
+                    return View();
+                }
+
+                userList.Remove(userFound);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
