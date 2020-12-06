a22dc3f70fe797419438d893a554f2c38f534c42
jhoanseb <jhlozano99@gmail.com>
2020-12-06 04:08:24 -0500

    Primera entrega

---
+++ Proyecto/WebDev/WebDev.Services/Entities/LoginDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDev.Services.Entities
+{
+    public class LoginDto
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public LoginDto()
+        {
+
+        }
+
+        public static LoginDto Build(string _email,string _password)
+        {
+            return new LoginDto
+            {
+                email = _email,
+                password = _password
+            };
+        }
+
+    }
+}
+
---
+++ Proyecto/WebDev/WebDev.Services/Entities/SerializableDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebDev.Services.Entities
+{
+    public class SerializableDto
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
