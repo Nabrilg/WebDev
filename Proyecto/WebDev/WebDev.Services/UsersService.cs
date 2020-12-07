e4bec689388e462fbf491746760ebde2278aff59
Nicortiz7272 <nicortiz@javerianacali.edu.co@>
2020-12-07 15:37:19 -0500

    Using restSharp to Get request

--- Proyecto/WebDev/WebDev.Services/UsersService.cs
+++ Proyecto/WebDev/WebDev.Services/UsersService.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using WebDev.Services.Entities;
+using RestSharp;
+using System.Net;
 
 namespace WebDev.Services
 {
@@ -14,33 +16,61 @@ namespace WebDev.Services
         private HttpClient _httpClient;
         private HttpClientHandler _httpClientHandler;
         private string BaseUrl { get; }
+        private readonly RestClient _restClient;
+        public TokenDto TokenDto { get; set; }
         public UsersService(string baseUrl)
         {
             BaseUrl = baseUrl;
             _httpClientHandler = new HttpClientHandler();
             _httpClientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
             _httpClient = new HttpClient(_httpClientHandler);
-            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer123");
-            SetupHttpConnection(_httpClient, baseUrl);
+            SetupHttpConnection(baseUrl);
+            _restClient = new RestClient();
         }
 
-        private void SetupHttpConnection(HttpClient httpClient, string baseUrl)
+        private void SetupHttpConnection(string baseUrl)
         {
             //Passing service base url  
-            httpClient.BaseAddress = new Uri(baseUrl);
+            _httpClient.BaseAddress = new Uri(baseUrl);
 
-            httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Clear();
 
             //Define request data format  
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer123");
         }
 
         public async Task<List<UserDto>> GetUsers()
         {
             var usersList = new List<UserDto>();
 
+            //Rest with RestSharp
+            // Assign the URL
+            _restClient.BaseUrl = new Uri($"{BaseUrl}users");
+
+            // Wait until to get a response
+            _restClient.Timeout = -1;
+
+            // Assign the Method Type
+            var request = new RestRequest(Method.GET);
+
+            // Assign the Headers
+            request.AddHeader("Authorization", "");
+            request.AddHeader("Content-Type", "application/json");
+
+            // Execute the Call
+            IRestResponse response = await _restClient.ExecuteAsync(request);
+            /// End Rest with RS
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                // Storing the content response recieved from web api
+                var responseContent = response.Content;
+
+                //Deserializing the response recieved from web api and storing into the Employee list
+                usersList = JsonConvert.DeserializeObject<List<UserDto>> (responseContent);
+            }
+            /*
             // Sending request to find web api REST service resource to Get All Users using HttpClient
-            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer");
             HttpResponseMessage response = await _httpClient.GetAsync($"users");
 
             // Checking the response is successful or not which is sent using HttpClient
@@ -52,6 +82,7 @@ namespace WebDev.Services
                 //Deserializing the response recieved from web api and storing into the Employee list
                 usersList = JsonConvert.DeserializeObject<List<UserDto>>(responseContent);
             }
+            */
 
             return usersList;
         }
@@ -60,8 +91,33 @@ namespace WebDev.Services
         {
             UserDto user = null;
 
+            //Rest with RestSharp
+            // Assign the URL
+            _restClient.BaseUrl = new Uri($"{BaseUrl}users/{id}");
+
+            // Wait until to get a response
+            _restClient.Timeout = -1;
+
+            // Assign the Method Type
+            var request = new RestRequest(Method.GET);
+
+            // Assign the Headers
+            request.AddHeader("Authorization", "");
+            request.AddHeader("Content-Type", "application/json");
+
+            // Execute the Call
+            IRestResponse response = await _restClient.ExecuteAsync(request);
+            /// End Rest with RS
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                // Storing the content response recieved from web api
+                var responseContent = response.Content;
+
+                //Deserializing the response recieved from web api and storing into the Employee list
+                user = JsonConvert.DeserializeObject<UserDto>(responseContent);
+            }
+            /*
             // Sending request to find web api REST service resource to Get All Users using HttpClient
-            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer");
             HttpResponseMessage response = await _httpClient.GetAsync($"users/{id}");
 
             // Checking the response is successful or not which is sent using HttpClient
@@ -73,7 +129,7 @@ namespace WebDev.Services
                 //Deserializing the response recieved from web api and storing into the Employee list
                 user = JsonConvert.DeserializeObject<UserDto>(responseContent);
             }
-
+            */
             return user;
         }
 
@@ -85,7 +141,6 @@ namespace WebDev.Services
             StringContent content = new StringContent(JsonConvert.SerializeObject(_user), Encoding.UTF8, "application/json");
 
             // Sending request to find web api REST service resource to Add an User using HttpClient
-            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer");
             HttpResponseMessage response = await _httpClient.PostAsync($"users", content);
 
             // Checking the response is successful or not which is sent using HttpClient
@@ -109,7 +164,6 @@ namespace WebDev.Services
             StringContent content = new StringContent(JsonConvert.SerializeObject(_user), Encoding.UTF8, "application/json");
 
             // Sending request to find web api REST service resource to Add an User using HttpClient
-            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer");
             HttpResponseMessage response = await _httpClient.PutAsync($"users/{user.Id}", content);
 
             // Checking the response is successful or not which is sent using HttpClient
