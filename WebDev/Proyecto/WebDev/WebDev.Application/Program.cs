8077939471d76cfa099fedf9d9400f188dd87fa0
jeisongarces19 <jeisongarces@javerianacali.edu.co>
2020-12-10 01:48:52 -0500

    Update Program.cs

--- WebDev/Proyecto/WebDev/WebDev.Application/Program.cs
+++ WebDev/Proyecto/WebDev/WebDev.Application/Program.cs
@@ -1,24 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
-namespace WebDev.Application
+namespace WebDev.Api
 {
-  public class Program
-  {
-    protected Program()
+    public class Program
     {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
-    public static void Main(string[] args)
-    {
-      CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
 }
