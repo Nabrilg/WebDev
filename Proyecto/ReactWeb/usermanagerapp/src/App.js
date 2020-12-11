c04f2a6df82de8a20819505a30dbf19d816adeb0
Maria Paula Carrero <mariapcarrero1@gmail.com>
2020-12-11 00:22:03 -0500

    Usermanagerapp working with Kotlin API

--- Proyecto/ReactWeb/usermanagerapp/src/App.js
+++ Proyecto/ReactWeb/usermanagerapp/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Layout } from './Layout';
+import { NoMatch } from './components/NoMatch';
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { List } from './components/Users';
+import { NavigationBar } from './components/NavigationBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <NavigationBar />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/Home" component={Home} />
+              <Route path="/Users" component={List} />
+              <Route path="/Login" component={Login} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
     </div>
   );
 }
